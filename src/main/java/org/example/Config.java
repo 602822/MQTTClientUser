c3a1aa0a8af6ae3ea060945d4135657f6aa79cc0
602822 <89077126+602822@users.noreply.github.com>
2026-03-05 10:37:21 +0100

    the hivemq broker and keycloak now runs inside the vm, changed the urls

--- src/main/java/org/example/Config.java
+++ src/main/java/org/example/Config.java
@@ -3,7 +3,7 @@ package org.example;
 // Used for non-sensitive data
 public class Config {
     final static String KEYCLOAK_REALM = "smartocean-testrealm";
-    final static String BASE_URL = "http://localhost:8080";
-    final static String BROKER_URL = "tcp://localhost:1883";
+    final static String BASE_URL = "http://158.39.77.107:8081";
+    final static String BROKER_URL = "tcp://158.39.77.107:1883";
 
 }
